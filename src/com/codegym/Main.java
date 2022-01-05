ab1ae718593d396174ac893323f9633a5afd6418
Hungyuuki <hungyuuki777@gmail.com>
2022-01-05 16:10:58 +0700

    1

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,30 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number = 0;
+        while (number < 100) {
+            if (number < 2) {
+                number++;
+                continue;
+            } else if (number == 2) {
+                System.out.println(number);
+            } else {
+                boolean isPrime = true;
+                for (int i = 2; i <= Math.sqrt(number); i++) {
+                    if (number % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime) {
+                    System.out.println(number);
+                }
+            }
+            number++;
+        }
+    }
+}
